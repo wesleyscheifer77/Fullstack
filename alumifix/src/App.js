9e90d4b760cc9b28d35451d2edbfb508929f58c1
wesleyscheifer77 <61853141+wesleyscheifer77@users.noreply.github.com>
2021-11-02 21:14:34 -0300

    Add files via upload

---
+++ alumifix/src/App.js
@@ -0,0 +1,83 @@
+
+import './App.css';
+/*import {Link} from 'react-router-dom';*/
+function App() {
+  return (
+    <div className="App">
+      <div className="main">
+        <div className="center">
+        <div className="menu">
+          <div className="logo">
+            </div>
+            <div className="item-menu">
+              <a href='#'>LOGIN</a>
+              <a href="#">CADASTRAR</a>
+              <a href="#">GARANTIA</a>
+              <a href="#">SOBRE-NÓS</a>
+              <a href="#">FALE-CONOSCO</a>
+              <a href="#">CARRINHO</a>
+              
+            </div>
+            </div>
+    </div>
+    </div>
+    <div className="pesquisa">
+      <input type="search" id="texto" list="historico" placeholder="Busque aqui"/>
+    <img src="https://png.pngtree.com/png-vector/20190329/ourlarge/pngtree-vector-magnifying-glass-icon-png-image_889405.jpg"className="btn" />
+
+    </div>
+    <div>
+    <h2>oiii</h2>
+    <h2>oiii</h2>
+    <h2>oiii</h2>
+    <h2>oiii</h2>
+    <h2>oiii</h2>
+    <h2>li ou ul</h2>
+    <h3>div tabela</h3>
+
+
+    </div>
+    <footer className="rodape_final"> 
+    
+    <article className="art_rod">
+      <img src="https://alumifixsolar.com.br/wp-content/uploads/2020/10/logo_site.png" className="img-roda"></img>
+    <article><p className="siga">siga-nos:</p>
+    <img src="https://img.icons8.com/fluency/48/000000/instagram-new.png"/>
+    <img src="https://img.icons8.com/fluency/48/000000/twitter.png"/>
+    <img src="https://img.icons8.com/color/48/000000/youtube-play.png"/>
+    <img src="https://img.icons8.com/fluency/48/000000/facebook.png"/>
+    </article>
+    </article>
+    <article className="art_rod center">
+    <article className="venda">
+      <img src= "https://img.icons8.com/office/25/000000/whatsapp--v1.png" className="img_whats"/>
+      </article>
+      <article className="vendas">
+      <p>WhatsApp Vendas</p>
+    <p>Allison (41) 9 9777 1655</p>
+<p>Eduardo (41) 9 9821 0384</p>
+<p>Elisabet (41) 9 9866 0143</p>
+<p>Fernanda (41) 9 9931 0263</p>
+<p>Ricardo (41) 9 9647 0068</p>
+<p>Roberto (41) 9 9972 0943</p>
+<p>Yusbeilit (41) 9 9647 0356</p>
+</article>
+    </article>
+    <article className="art_rod center">
+    <article className="venda">
+      <img src= "https://img.icons8.com/office/25/000000/whatsapp--v1.png" className="img_whats"/>
+      </article>
+      <article className="vendas">
+      <p>WhatsApp Vendas</p>
+      <p>Italo (41) 9 9757 3478</p>
+      <p>João (41)9 9647 0332</p>
+      <p>WhatsApp Gerente Comercial</p>
+      <p>William (41) 9 9931 0272</p>
+</article>
+    </article>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
